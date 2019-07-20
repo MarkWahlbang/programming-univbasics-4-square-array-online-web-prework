7f739e056cf8a10f144b74050b7eceac5f904e2a
brady sisk <bradysiskpro@yahoo.com>
2019-07-20 19:53:58 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,8 +1,8 @@
 def square_array(array)
- **(2) = **(2)
+ ** = **(2)
 numbers = [1,2,3,4,5]
  
-while numbers[**(2)] do
-  puts numbers[**(2)]
+while numbers[**] do
+  puts numbers[**]
   ** = (**(2))
 end
